9b0f4af7b6dcfcb4b6f832b1a6e8c00b5ebf3753
Tushar Dukane <tushardukane@Tushars-MacBook-Air.local>
2023-11-25 00:53:56 +0530

    first commit

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.DATABASE || 3001;
+
 
 // dotenv config
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "./.env" });
+
 
 // URL of database
-
 const URL = process.env.DATABASE;
+
+const PORT = process.env.DATABASE || 3001;
+
 
 // MongoDB connection
 
